2a4cedcba49aa9fb212ca42e51898ffedbe1c4c1
alex <alex.shustrov@gmail.com>
2016-09-07 01:45:24 +0300

    init commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,12 @@
+
 module.exports = {
-    entry: __dirname + "./src/index.js",
+    entry: "./src/index.js",
     output: {
         path: __dirname,
         filename: "./public/app.js"
     },
     module: {
-            loaders: [
+        loaders: [
             {
                 test: /\.js$/,
                 exclude: /(node_modules|bower_components)/,
